32c892edc8bbedd0069520c5419e2cbf9cbc1671
mrafidianto <rafidianto.code@gmail.com>
2020-01-27 14:56:32 +0700

    Updates

---
+++ precache-manifest.0d2488daa5d4d0667e0bc825c8c338b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f179fb4485dd5739835b5517d29d9828",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "f785b0139908d14fc7e9",
+    "url": "/monsters-roladex/static/css/main.c8e6866a.chunk.css"
+  },
+  {
+    "revision": "97782ef9a5d04a8a2f71",
+    "url": "/monsters-roladex/static/js/2.d5e2d5a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-roladex/static/js/2.d5e2d5a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f785b0139908d14fc7e9",
+    "url": "/monsters-roladex/static/js/main.78ed5c75.chunk.js"
+  },
+  {
+    "revision": "138502b632be313ada6e",
+    "url": "/monsters-roladex/static/js/runtime-main.73e4e9cb.js"
+  }
+]);
